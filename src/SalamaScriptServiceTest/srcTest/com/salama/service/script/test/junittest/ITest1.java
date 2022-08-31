424fd31c1c86893b7fde1ef89a3bc0a3c8c2436c
Beef Liu <beef.liu@gmail.com>
2022-08-31 17:15:06 +0800

    convert the old eclipse project into maven project

--- src/SalamaScriptServiceTest/srcTest/com/salama/service/script/test/junittest/ITest1.java
+++
@@ -1,8 +0,0 @@
-package com.salama.service.script.test.junittest;
-
-public interface ITest1 {
-
-    String getName();
-    
-    String doJob(String jobName);
-}
--- src/TestScriptSourceContainer/script_root/script_app_src/app1/service/TestInternalService.js
+++
@@ -1,8 +0,0 @@
-TestInternalService = new (function () {
-    var logger = LoggerUtil.getLogger($getApp() + "/" + "TestInternalService" + ".js");
-
-    this.run = function(params) {
-        logger.debug("TestInternalService -----------------");
-    };
-
-});
